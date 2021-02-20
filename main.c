a4290d348b4e7a06141289b210d11a737b05d5e8
swati155 <72041157+swati155@users.noreply.github.com>
2021-02-20 23:12:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int week;
+    printf("Enter week one\n");
+    scanf("%d",&week);
+    if(week==1)
+    {
+        printf("monday\n");
+    }
+    printf("Enter week two\n");
+    scanf("%d",&week);
+    if(week==2)
+    {
+        printf("tuesday\n");
+    }
+    printf("Enter week three\n");
+    scanf("%d",&week);
+    if(week==3)
+    {
+        printf("wednesday\n");
+    }
+    printf("Enter week four\n");
+    scanf("%d",&week);
+    if(week==4)
+    {
+        printf("thursday\n");
+    }
+    printf("Enter week five\n");
+    scanf("%d",&week);
+    if(week==5)
+    {
+        printf("friday\n");
+    }
+    printf("Enter week six\n");
+    scanf("%d",&week);
+    if(week==6)
+    {
+        printf("saturday\n");
+    }
+    printf("Enter week seven\n");
+    scanf("%d",&week);
+    if(week==7)
+    {
+        printf("sunday\n");
+    }
+    printf("this is the week with his day");
+    return 0;
+}
